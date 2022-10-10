5bf68221950f37d9c18093c7d543ba5a521f4c0c
Kishore-14 <114301280+Kishore-14@users.noreply.github.com>
2022-10-10 13:29:27 +0530

    Add files via upload

---
+++ Matrix validation.c.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{	
+	printf("To check\n1.All diagonal elements are postive"
+	"\n");
+	int i,j,m,n,a[10][10];
+	printf("\nEnter the number of rows and columns : ");
+	scanf("%d%d",&m,&n);
+	if(m!=n)
+	{
+		printf("\nEnter the valid inputs : ");
+		scanf("%d%d",&m,&n);
+	}
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("\nElement %d%d : ",i+1,j+1);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<m;i++)
+	{
+		if(a[i][i]<0)
+		printf("\nAll diagonals are not positive");
+	}
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i!=j)
+			{
+				if(a[i][j]>0)
+				printf("\nAll non diagonal elements are not negative ");
+			}
+			break;
+		}
+	}
+}
